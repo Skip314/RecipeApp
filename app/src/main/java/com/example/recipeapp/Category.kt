74d0b68f6119c6ad81fccb66fd2e8b204c5689b7
Afana <afanasevlad@mail.ru>
2024-06-10 16:19:28 +0300

    RA-06 create local db

--- app/src/main/java/com/example/recipeapp/Category.kt
+++ app/src/main/java/com/example/recipeapp/Category.kt
@@ -1,20 +1,21 @@
 package com.example.recipeapp
 
 class Category(
-    val id: String,
+    val id: Int,
     val title: String,
     val description: String,
     val imageUrl: String,
 )
 
 class Recipe(
-    val name: String,
+    val id: Int,
+    val title: String,
     val quality: Int,
     val ingredients: List<Ingredient>,
-    val cooking: String
 )
 
 class Ingredient(
-    val name: String,
-    val quality: Int
+    val quality: Float,
+    val unitOfMeasure: String,
+    val description: String,
 )
